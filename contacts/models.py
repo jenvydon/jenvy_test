a3ccb3ab9d1dd01203dd611cbce7b40481b580b8
jenvydon <jenvy.don@gmail.com>
2012-10-11 23:25:00 +0300

    Create models.py. Add Makefile

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Contacts(models.Model):
+    email = models.EmailField()
+    jid = models.EmailField()
+    skype = models.CharField(max_length=50)
+    other = models.TextField()
+
+class Bio(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date_of_birth = models.DateField('Date birth')
+    about = models.TextField()
+    contacts = models.ForeignKey(Contacts)
+
+    def __unicode__(self):
+        return self.name
